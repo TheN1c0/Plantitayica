465b4552b75d5f485470a7e7450dbef97b6a839c
Nicolas Bravo <nicojjj4@gmail.com>
2024-05-16 21:42:38 -0400

    correcciones

--- pagina.js
+++ pagina.js
@@ -1,6 +1,6 @@
 function mostrarContenido(id) {
     // Oculta todos los contenidos
-    
+    alert("presionado");
     /* queryselectorall selecciona varios ids, ^ hace que seleccione todos los ides que comiencen con "contenido" */
    /*  La función itera sobre todos los elementos seleccionados usando forEach 
     y establece el estilo display de cada elemento en none, lo que los oculta visualmente en la página. */
