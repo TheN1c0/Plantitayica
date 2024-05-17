b424bb7c44568b312f599dd654089982d7eb8e52
Nicolas Bravo <nicojjj4@gmail.com>
2024-05-16 22:32:54 -0400

    Se agregó fondo imagen

--- pagina.js
+++ pagina.js
@@ -1,6 +1,7 @@
 function mostrarContenido(id) {
     // Oculta todos los contenidos
     alert("presionado");
+    alert(id);
     /* queryselectorall selecciona varios ids, ^ hace que seleccione todos los ides que comiencen con "contenido" */
    /*  La función itera sobre todos los elementos seleccionados usando forEach 
     y establece el estilo display de cada elemento en none, lo que los oculta visualmente en la página. */
